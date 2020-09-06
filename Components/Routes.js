b089672962b0723bb2dcfb7af1d4a581150b0398
Augustin Jose <augustinjose1221@gmail.com>
2020-09-06 14:44:35 +0530

    Minor Updates

--- Components/Routes.js
+++ Components/Routes.js
@@ -3,6 +3,7 @@ import { Router, Scene } from 'react-native-router-flux'
 import Home from '../Pages/Home.js'
 import SignUp from '../Pages/SignUp.js'
 import Login from '../Pages/Login.js'
+import Chat from '../Pages/Chat.js'
 
 const Routes = () => (
    <Router>
@@ -10,6 +11,7 @@ const Routes = () => (
          <Scene key = "home" component = {Home} title = "Home" initial = {true} />
          <Scene key = "SignUp" component = {SignUp} title = "SignUp" />
          <Scene key = "Login" component = {Login} title = "Login" />
+         <Scene key = "Chat" component = {Chat} title = "Chat" />
       </Scene>
    </Router>
 )
